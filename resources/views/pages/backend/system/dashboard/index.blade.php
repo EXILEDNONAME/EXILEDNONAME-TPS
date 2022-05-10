fece468de39db511e138f4e94ff7534399c34cd7
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-05-10 12:00:39 +0700

    Update

--- resources/views/pages/backend/system/dashboard/index.blade.php
+++ resources/views/pages/backend/system/dashboard/index.blade.php
@@ -49,50 +49,242 @@
             <path d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"></path>
             <path d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z" fill="#000000" fill-rule="nonzero"></path>
           </g>
-        </svg><!--end::Svg Icon--></span>
-      </div>
-      <div class="alert-text">
-        <span class="font-weight-bold"> Total Users </span><br>
-        <a href="/dashboard/management/users" class="text-danger"> 4 </a> Users
-      </div>
+        </svg>
+      </span>
     </div>
-  </div>
-  <div class="col-md-3">
-    <div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
-      <div class="alert-icon">
-        <span class="svg-icon svg-icon-danger svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo1\dist/../src/media/svg/icons\General\Lock.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <mask fill="white">
-              <use xlink:href="#path-1"></use>
-            </mask>
-            <g></g>
-            <path d="M7,10 L7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 L17,10 L18,10 C19.1045695,10 20,10.8954305 20,12 L20,18 C20,19.1045695 19.1045695,20 18,20 L6,20 C4.8954305,20 4,19.1045695 4,18 L4,12 C4,10.8954305 4.8954305,10 6,10 L7,10 Z M12,5 C10.3431458,5 9,6.34314575 9,8 L9,10 L15,10 L15,8 C15,6.34314575 13.6568542,5 12,5 Z" fill="#000000"></path>
-          </g>
-        </svg><!--end::Svg Icon--></span>
-      </div>
-      <div class="alert-text">
-        <span class="font-weight-bold"> Total Accesses </span><br>
-        <a href="/dashboard/management/accesses" class="text-danger"> 3 </a> Accesses
-      </div>
+    <div class="alert-text">
+      <b> TOTAL RW </b>, <br>
+      <span class="text-muted"> {{ \DB::table('tps_location_rws')->get()->count(); }} </span>
     </div>
   </div>
 </div>
+<div class="col-md-3">
+  <div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
+    <div class="alert-icon">
+      <span class="svg-icon svg-icon-danger svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo1\dist/../src/media/svg/icons\General\Lock.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <mask fill="white">
+            <use xlink:href="#path-1"></use>
+          </mask>
+          <g></g>
+          <path d="M7,10 L7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 L17,10 L18,10 C19.1045695,10 20,10.8954305 20,12 L20,18 C20,19.1045695 19.1045695,20 18,20 L6,20 C4.8954305,20 4,19.1045695 4,18 L4,12 C4,10.8954305 4.8954305,10 6,10 L7,10 Z M12,5 C10.3431458,5 9,6.34314575 9,8 L9,10 L15,10 L15,8 C15,6.34314575 13.6568542,5 12,5 Z" fill="#000000"></path>
+        </g>
+      </svg>
+    </span>
+  </div>
+  <div class="alert-text">
+    <b> TOTAL RT </b>, <br>
+    <span class="text-muted"> {{ \DB::table('tps_location_rts')->get()->count(); }} </span>
+  </div>
+</div>
+</div>
+</div>
 
-DAPIL 1 : <br>
-{{ \DB::table('tps_location_districts')->where('id_area', 1)->get()->count(); }} KECAMATAN <br>
-@php $data_1 = \DB::table('tps_location_districts')->where('id_area', 1)->get(); @endphp
-@foreach($data_1 as $data_1)
-@php $data_2 = \DB::table('tps_location_villages')->where('id_district', $data_1->id)->get(); @endphp
-@foreach($data_2 as $data_2)
-@php $data_3 = \DB::table('tps_location_rws')->where('id_village', $data_2->id)->get(); @endphp
-KECAMATAN {{ $data_1->name }} - {{ $data_2->name }} -
-{{ $data_3->count() }} RW <br>
-@endforeach
-@endforeach
+<div class="row">
+  <div class="col-md-4">
+    <div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
 
-<hr>
+      <div class="alert-text">
 
-JUMLAH DAPIL : {{ \DB::table('tps_location_areas')->get()->count(); }} <br>
-JUMLAH KECAMATAN : {{ \DB::table('tps_location_districts')->get()->count(); }} <br>
-JUMLAH KELURAHAN : {{ \DB::table('tps_location_villages')->get()->count(); }} <br>
-@endpush
+        <center class="font-weight-bold">
+          - AREA 1 - <br> 129 Participant
+        </center>
+        <hr>
+
+        <table width="100%" class="table table-bordered table-hover" id="exilednoname" style="width: 100%;">
+          <thead>
+            <th> KELURAHAN </th>
+            <th class="text-center"> RW </th>
+            <th class="text-center"> RT </th>
+          </thead>
+          <tbody>
+            <tr>
+              @php
+              $rt = 0; $rw = 0; for ($i = 1; $i <= 6; $i++) {
+                $rw += \DB::table('tps_location_rws')->where('id_village', $i)->get()->count();
+                $rt += \DB::table('tps_location_rts')->where('id_village', $i)->get()->count();
+              }
+              @endphp
+              <td> ANDIR </td>
+              <td class="text-center"> {{ $rw }} </td>
+              <td class="text-center"> {{ $rt }} </td>';
+            </tr>
+            <tr>
+              @php
+              $rt = 0; $rw = 0; for ($i = 7; $i <= 12; $i++) {
+                $rw += \DB::table('tps_location_rws')->where('id_village', $i)->get()->count();
+                $rt += \DB::table('tps_location_rts')->where('id_village', $i)->get()->count();
+              }
+              @endphp
+              <td> CICENDO </td>
+              <td class="text-center"> {{ $rw }} </td>
+              <td class="text-center"> {{ $rt }} </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
+
+      <div class="alert-text">
+        <table width="100%" class="table table-bordered table-hover" id="exilednoname" style="width: 100%;">
+          <thead>
+            <th> KELURAHAN </td>
+              <th> RW </td>
+                <th> RT </td>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td> ANDIR </td>
+                    <td> </td>
+                    <td> 381 </td>
+                  </tr>
+                  <tr>
+                    <td> ANDIR </td>
+                    <td> 54 </td>
+                    <td> 381 </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
+
+            <div class="alert-text">
+              <table width="100%" class="table table-bordered table-hover" id="exilednoname" style="width: 100%;">
+                <thead>
+                  <th> KELURAHAN </td>
+                    <th> RW </td>
+                      <th> RT </td>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td> ANDIR </td>
+                          <td> 54 </td>
+                          <td> 381 </td>
+                        </tr>
+                        <tr>
+                          <td> ANDIR </td>
+                          <td> 54 </td>
+                          <td> 381 </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-3">
+                <div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
+                  <div class="alert-icon">
+                    <span class="svg-icon svg-icon-success">
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <rect x="0" y="0" width="24" height="24"></rect>
+                          <circle fill="#000000" cx="9" cy="15" r="6"></circle>
+                          <path d="M8.8012943,7.00241953 C9.83837775,5.20768121 11.7781543,4 14,4 C17.3137085,4 20,6.6862915 20,10 C20,12.2218457 18.7923188,14.1616223 16.9975805,15.1987057 C16.9991904,15.1326658 17,15.0664274 17,15 C17,10.581722 13.418278,7 9,7 C8.93357256,7 8.86733422,7.00080962 8.8012943,7.00241953 Z" fill="#000000" opacity="0.3"></path>
+                        </g>
+                      </svg>
+                    </span>
+                  </div>
+                  <div class="alert-text">
+                    @php
+                    $rw = 0; $rt = 0;
+                    for ($i = 1; $i <= 6; $i++) { $rw += \DB::table('tps_location_rws')->where('id_village', $i)->get()->count(); }
+                    for ($i = 1; $i <= 6; $i++) { $rt += \DB::table('tps_location_rts')->where('id_village', $i)->get()->count(); }
+                    @endphp
+                    <b> ANDIR </b>,
+                    <span class="text-muted"> {{ $rw }} RW </span> - <span class="text-muted"> {{ $rt }} RT </span>
+
+                    @php
+                    $rw = 0; $rt = 0;
+                    for ($i = 7; $i <= 12; $i++) { $rw += \DB::table('tps_location_rws')->where('id_village', $i)->get()->count(); }
+                    for ($i = 7; $i <= 12; $i++) { $rt += \DB::table('tps_location_rts')->where('id_village', $i)->get()->count(); }
+                    @endphp
+                    <b> CICENDO </b>,
+                    <span class="text-muted"> {{ $rw }} RW </span> - <span class="text-muted"> {{ $rt }} RT </span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
+                  <div class="alert-icon">
+                    <span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo1\dist/../src/media/svg/icons\Communication\Mail-opened.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <rect x="0" y="0" width="24" height="24"></rect>
+                        <path d="M6,2 L18,2 C18.5522847,2 19,2.44771525 19,3 L19,12 C19,12.5522847 18.5522847,13 18,13 L6,13 C5.44771525,13 5,12.5522847 5,12 L5,3 C5,2.44771525 5.44771525,2 6,2 Z M7.5,5 C7.22385763,5 7,5.22385763 7,5.5 C7,5.77614237 7.22385763,6 7.5,6 L13.5,6 C13.7761424,6 14,5.77614237 14,5.5 C14,5.22385763 13.7761424,5 13.5,5 L7.5,5 Z M7.5,7 C7.22385763,7 7,7.22385763 7,7.5 C7,7.77614237 7.22385763,8 7.5,8 L10.5,8 C10.7761424,8 11,7.77614237 11,7.5 C11,7.22385763 10.7761424,7 10.5,7 L7.5,7 Z" fill="#000000" opacity="0.3"></path>
+                        <path d="M3.79274528,6.57253826 L12,12.5 L20.2072547,6.57253826 C20.4311176,6.4108595 20.7436609,6.46126971 20.9053396,6.68513259 C20.9668779,6.77033951 21,6.87277228 21,6.97787787 L21,17 C21,18.1045695 20.1045695,19 19,19 L5,19 C3.8954305,19 3,18.1045695 3,17 L3,6.97787787 C3,6.70173549 3.22385763,6.47787787 3.5,6.47787787 C3.60510559,6.47787787 3.70753836,6.51099993 3.79274528,6.57253826 Z" fill="#000000"></path>
+                      </g>
+                    </svg><!--end::Svg Icon--></span>
+                  </div>
+                  <div class="alert-text">
+                    <b> TOTAL KELURAHAN </b>, <br>
+                    <span class="text-muted"> {{ \DB::table('tps_location_villages')->get()->count(); }} </span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
+                  <div class="alert-icon">
+                    <span class="svg-icon svg-icon-danger svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo1\dist/../src/media/svg/icons\General\User.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <polygon points="0 0 24 0 24 24 0 24"></polygon>
+                        <path d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"></path>
+                        <path d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z" fill="#000000" fill-rule="nonzero"></path>
+                      </g>
+                    </svg>
+                  </span>
+                </div>
+                <div class="alert-text">
+                  <b> TOTAL RW </b>, <br>
+                  <span class="text-muted"> {{ \DB::table('tps_location_rws')->get()->count(); }} </span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="alert alert-custom alert-white alert-shadow fade show gutter-b" role="alert">
+                <div class="alert-icon">
+                  <span class="svg-icon svg-icon-danger svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo1\dist/../src/media/svg/icons\General\Lock.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <mask fill="white">
+                        <use xlink:href="#path-1"></use>
+                      </mask>
+                      <g></g>
+                      <path d="M7,10 L7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 L17,10 L18,10 C19.1045695,10 20,10.8954305 20,12 L20,18 C20,19.1045695 19.1045695,20 18,20 L6,20 C4.8954305,20 4,19.1045695 4,18 L4,12 C4,10.8954305 4.8954305,10 6,10 L7,10 Z M12,5 C10.3431458,5 9,6.34314575 9,8 L9,10 L15,10 L15,8 C15,6.34314575 13.6568542,5 12,5 Z" fill="#000000"></path>
+                    </g>
+                  </svg>
+                </span>
+              </div>
+              <div class="alert-text">
+                <b> TOTAL RT </b>, <br>
+                <span class="text-muted"> {{ \DB::table('tps_location_rts')->get()->count(); }} </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        DAPIL 1 : <br>
+        {{ \DB::table('tps_location_districts')->where('id_area', 1)->get()->count(); }} KECAMATAN <br>
+        @php $data_1 = \DB::table('tps_location_districts')->where('id_area', 1)->get(); @endphp
+        @foreach($data_1 as $data_1)
+        @php $data_2 = \DB::table('tps_location_villages')->where('id_district', $data_1->id)->get(); @endphp
+        @foreach($data_2 as $data_2)
+        @php $data_3 = \DB::table('tps_location_rws')->where('id_village', $data_2->id)->get(); @endphp
+        KECAMATAN {{ $data_1->name }} - {{ $data_2->name }} -
+        {{ $data_3->count() }} RW <br>
+        @endforeach
+        @endforeach
+
+        <hr>
+
+        JUMLAH DAPIL : {{ \DB::table('tps_location_areas')->get()->count(); }} <br>
+        JUMLAH KECAMATAN : {{ \DB::table('tps_location_districts')->get()->count(); }} <br>
+        JUMLAH KELURAHAN : {{ \DB::table('tps_location_villages')->get()->count(); }} <br>
+        @endpush
